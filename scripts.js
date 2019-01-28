977d8cc9114ed86bec1b90fe1228e5e3803287d6
Stefan Formentano <StefanFormentano@gmail.com>
2019-01-28 07:12:00 -0600

    project complete

---
+++ scripts.js
@@ -0,0 +1,36 @@
+async function fetchGhilbli() {
+	try {
+	  const response = await fetch('https://ghibliapi.herokuapp.com/films')
+	  const data = await response.json();
+	    data.forEach(movie => {
+	      const card = document.createElement('div');
+	      card.setAttribute('class', 'card');
+
+	      const h1 = document.createElement('h1');
+	      h1.textContent = movie.title;
+
+	      const p = document.createElement('p');
+	      movie.description = movie.description.substring(0, 300);
+	      p.textContent = `${movie.description}...`;
+
+	      container.appendChild(card);
+	      card.appendChild(h1);
+	      card.appendChild(p);
+	    });
+	} catch (err) {
+		console.log('may all your bacon burn', err)
+	}
+ }
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+fetchGhilbli();
